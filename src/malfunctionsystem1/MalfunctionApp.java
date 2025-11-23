c0d826130ddde2c9ad3cb0701f8cdcc2334f56d4
LENOVO <LENOVO@DESKTOP-I8TVS9U>
2025-11-23 12:51:18 +0300

    Initial project upload

---
+++ src/malfunctionsystem1/MalfunctionApp.java
@@ -0,0 +1,14 @@
+
+package malfunctionsystem1;
+
+
+public class MalfunctionApp {
+
+  
+    public static void main(String[] args) {
+        System.out.println("\t\tWelcome to Malfunction Report System!\n"
+                + "\t_______________________________________________\n");
+
+    }
+    
+}
