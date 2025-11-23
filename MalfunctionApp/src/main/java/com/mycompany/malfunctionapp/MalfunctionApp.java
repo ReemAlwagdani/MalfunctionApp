e74752b1abd6db6feb0d9d86a1bf785be213c34c
janasbw <jana.sbw@gmail.com>
2025-11-23 21:52:30 +0300

    Remove incorrect code

--- MalfunctionApp/src/main/java/com/mycompany/malfunctionapp/MalfunctionApp.java
+++ MalfunctionApp/src/main/java/com/mycompany/malfunctionapp/MalfunctionApp.java
@@ -1,16 +1,1 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
-package com.mycompany.malfunctionapp;
-
-/**
- *
- * @author jansb
- */
-public class MalfunctionApp {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
